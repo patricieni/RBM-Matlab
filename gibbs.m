9a5c95291f40c969e49fb64f930c8ce1145a0281
patricieni <patric.fulop@gmail.com>
2016-11-19 23:37:05 -0800

    Add probability when sampling not units

--- gibbs.m
+++ gibbs.m
@@ -2,14 +2,13 @@ function v = gibbs(v1,h_bias,v_bias,W,steps)
 
 for i = 1:steps
     hidden_p = sigmoid(v1*W+h_bias);   
-    h = zeros(size(hidden_p));                           
-    h(hidden_p >= rand(size(hidden_p))) = 1;   
+    %h = zeros(size(hidden_p));                           
+    %h(hidden_p >= rand(size(hidden_p))) = 1;   
     
-    visible_p = sigmoid(h*W'+v_bias);
+    visible_p = sigmoid(hidden_p*W'+v_bias);
     v = zeros(size(visible_p));                           
+    v(visible_p >=rand(size(visible_p))) = 1;
     
-    condition = ((v1 == 1) | ((v1 == 0.5) & visible_p >= rand(size(visible_p))));
-    v(condition) = 1;
     v1 = v;
 end
 
